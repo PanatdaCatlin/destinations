eb1b122dc89f5a97da517427c50da1ad4283ba4a
Justin Lardani and Panatda Inthala <justin@>
2018-01-22 16:01:33 -0800

    adds more styling and toggles.

--- js/scripts.js
+++ js/scripts.js
@@ -26,17 +26,23 @@ $(document).ready(function() {
   var displayNewDestination = function() {
     var lastObject = destinationsArray[destinationsArray.length - 1];
 
-    $("#destinationsContainer").append('<div class="col-md-4"></div>');
+    $("#destinationsContainer").append('<div class="col-md-4"><div class="destinationsCard"></div></div>');
 
-    $("#destinationsContainer .col-md-4:last-child").append('<h2 class="destinationName clickToExpand">' + lastObject.location + '</h2>');
-    $("#destinationsContainer .col-md-4:last-child").append('<img class="destinationImage">')
-    $("#destinationsContainer .col-md-4:last-child img").attr("src", lastObject.imageURL)
-    $("#destinationsContainer .col-md-4:last-child").append('<p class="landmarks">' + lastObject.landmarks + '</p>');
-    $("#destinationsContainer .col-md-4:last-child").append('<p class="season">' + lastObject.season + '</p>');
-    $("#destinationsContainer .col-md-4:last-child").append('<p class="notes">' + lastObject.info + '</p>');
+    $("#destinationsContainer .col-md-4:last-child .destinationsCard").append('<h2 class="destinationName clickToExpand">' + lastObject.location + '</h2>');
+    $("#destinationsContainer .col-md-4:last-child .destinationsCard").append('<img class="destinationImage">')
+    $("#destinationsContainer .col-md-4:last-child .destinationsCard img").attr("src", lastObject.imageURL)
+    $("#destinationsContainer .col-md-4:last-child .destinationsCard").append('<p class="landmarks">' + lastObject.landmarks + '</p>');
+    $("#destinationsContainer .col-md-4:last-child .destinationsCard").append('<p class="season">' + lastObject.season + '</p>');
+    $("#destinationsContainer .col-md-4:last-child .destinationsCard").append('<p class="notes">' + lastObject.info + '</p>');
+    $("#destinationsContainer .col-md-4:last-child .destinationsCard").append('<button class="delete btn">Delete</button>');
 
-    $(".col-md-4:last-child > .clickToExpand").click(function() {
-      $(this).parent().find('img, p').toggle();
+    $(".delete").click(function() {
+      $(this).parent().parent().hide();
+    })
+
+    $(".col-md-4:last-child > .destinationsCard > .clickToExpand").click(function() {
+      $(this).parent().find('img, p, button').slideToggle();
+      $(this).parent().find('img, button').toggleClass("centerBlock");
     });
   };
 
